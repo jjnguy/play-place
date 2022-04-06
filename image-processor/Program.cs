82399627e6a91980cc2946742c4e697a87c3c42a
Justin Nelson <justin.john.nelson@gmail.com>
2022-04-05 22:58:49 -0500

    ug

--- image-processor/Program.cs
+++ image-processor/Program.cs
@@ -1,11 +1,21 @@
-﻿
+﻿using System.Drawing;
 
 var imageSetString = File.ReadAllText("../image-downloader/images_index.json");
 var imageSet = System.Text.Json.JsonSerializer.Deserialize<ImageSet>(imageSetString);
 Console.WriteLine(imageSet.count);
 
+var sorted = imageSet.files.OrderBy(f => f.name).ToArray();
 
+// TODO: how do I deal with images?
+
+var previousImage = Graphics.FromImage(Image.FromFile("../image-downloader/out/" + sorted[0].name));
+foreach (var file in sorted)
+{
+  var img = Graphics.FromImage(Image.FromFile("../image-downloader/out/" + file.name));
+  for (var x = 0; x < img.)
+}
 
 
 record FileDescriptor(string name, string date);
 record ImageSet(int count, List<FileDescriptor> files);
+record PixelDiff(short x, short y, byte newColor);
